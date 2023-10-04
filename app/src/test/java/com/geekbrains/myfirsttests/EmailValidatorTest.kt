89ae1b5ba51e6356eb5956d9aa84cf8d920b71c7
UpdateShu <rilabsure@yandex.ru>
2023-10-04 11:29:08 +0300

    Добавлены негативные тесты

--- app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
@@ -40,4 +40,43 @@ class EmailValidatorTest {
     fun emailValidator_NullEmail_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(null))
     }
+
+    @Test
+    fun emailValidator_NoDomain_ReturnsFalse(){
+        assertFalse(EmailValidator.isValidEmail("nameemail.com"))
+    }
+
+    @Test
+    fun emailValidator_SpaceWithEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(" "))
+        assertFalse(EmailValidator.isValidEmail("name @email.com"))
+        assertFalse(EmailValidator.isValidEmail("name@e mail.com"))
+    }
+
+    @Test
+    fun emailValidator_SpecialChars_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name!@email.com"))
+        assertFalse(EmailValidator.isValidEmail("na*me@email.com"))
+        assertFalse(EmailValidator.isValidEmail("na^me@email.com"))
+        assertFalse(EmailValidator.isValidEmail("\$name@email.com"))
+        assertFalse(EmailValidator.isValidEmail("name@email.com?"))
+    }
+
+    @Test
+    fun emailValidator_NoLatins_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("ИмяПочты@email.com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidLength_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("Abcdefghijklmnopqrstuvwxyz1Abcdefghijklmnopqrstuvwxyz2Abcdefghijklmnopqrstuvwxyz3Abcdefghijklmnopqrstuvwxyz4Abcdefghijklmnopqrstuvwxyz5Abcdefghijklmnopqrstuvwxyz6Abcdefghijklmnopqrstuvwxyz7Abcdefghijklmnopqrstuvwxyz8Abcdefghijklmnopqrstuvwxyz9Abcdefghijklmnopqrstuvwxyz10@email.com"))
+        assertFalse(EmailValidator.isValidEmail("name@Abcdefghijklmnopqrstuvwxyz1Abcdefghijklmnopqrstuvwxyz2Abcdefghijklmnopqrstuvwxyz3.com"))
+        assertFalse(EmailValidator.isValidEmail("name@email.Abcdefghijklmnopqrstuvwxyz1"))
+    }
+
+    @Test
+    fun emailValidator_InvalidSubDomain_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email.com."))
+        assertFalse(EmailValidator.isValidEmail("name@email.com..."))
+    }
 }
